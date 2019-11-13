9c5afaeb6d210de0372a0f7c46c10a0c8f404a91
863416870 <863416870@qq.com>
2019-11-13 14:12:52 +0800

    [mod]第一题俩数之和 调整for 减少运行时间

--- Temp/leetcode/editor/cn/[1]两数之和.java
+++ Temp/leetcode/editor/cn/[1]两数之和.java
@@ -17,18 +17,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i],i);
-        }
-
-        for (int i = 0; i < nums.length; i++) {
-            int otherNum = target - nums[i];
-            if(map.containsKey(otherNum) && map.get(otherNum) != i){
-                return new int[]{i,map.get(otherNum)};
+        for (int i = 0; i < nums.length; ++i) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[] {map.get(target - nums[i]), i};
             }
+            map.put(nums[i], i);
         }
-        throw new IllegalArgumentException("No two sum solution");
-
+        return null;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
